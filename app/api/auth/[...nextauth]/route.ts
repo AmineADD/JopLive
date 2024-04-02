00ac2ddda863d66e838a56dd1c4e2d38d051e9ce
amine01dev <amine@sherpas.com>
2024-04-02 12:10:38 +0000

    :green_heart: Fix build export nestjs

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,23 +1,5 @@
-import nextAuth, { AuthOptions } from "next-auth";
-import GoogleProvider from "next-auth/providers/google";
-import { PrismaAdapter } from "@next-auth/prisma-adapter";
-
-import prisma from "@/app/libs/prismadb";
-
-export const authOptions: AuthOptions = {
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
-    }),
-  ],
-  debug: process.env.NODE_ENV === "development",
-  session: {
-    strategy: "jwt",
-  },
-  secret: process.env.NEXTAUTH_SECRET as string,
-};
+import nextAuth from "next-auth";
+import { authOptions } from "./auth";
 
 const handler = nextAuth(authOptions);
 
